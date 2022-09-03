4c1096289690804cca89caaa256a88c4bbf7f3fb
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-09-03 16:16:24 +0200

    feat: add airthings

--- src/main/kotlin/no/oyvindis/kafkaproducerclimate/KafkaProducerClimateApplication.kt
+++ src/main/kotlin/no/oyvindis/kafkaproducerclimate/KafkaProducerClimateApplication.kt
@@ -1,9 +1,13 @@
 package no.oyvindis.kafkaproducerclimate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
 class KafkaProducerClimateApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/no/oyvindis/kafkaproducerclimate/entities/Sensor.kt
+++ src/main/kotlin/no/oyvindis/kafkaproducerclimate/entities/Sensor.kt
@@ -1,10 +1,16 @@
 package no.oyvindis.kafkaproducerclimate.entities
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
+@JsonIgnoreProperties
 data class Sensor(
-    @JsonProperty("temperature")
-    val temperature: String,
-    @JsonProperty("humidity")
-    val humidity: String?
+    var location: String?,
+    val battery: String? = null,
+    val co2: String? = null,
+    val humidity: String? = null,
+    val pm1: String? = null,
+    val pm25: String? = null,
+    val pressure: String? = null,
+    val temp: String? = null,
+    val voc: String? = null
 )
